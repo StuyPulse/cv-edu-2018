77b84ea946e92cce0df8267b6606afae45f4340b
Winston Peng <wpeng00@stuy.edu>
2018-06-27 00:18:53 -0400

    Fix Main.java so that the image directory is actually correct, also add broken code to LessonZeroVision.java. In other news, I got my report card back :'(

--- src/LessonZeroVision.java
+++ src/LessonZeroVision.java
@@ -1,15 +1,48 @@
+import java.util.ArrayList;
+
 import stuyvision.VisionModule;
 import stuyvision.gui.VisionGui;
+import stuyvision.gui.IntegerSliderVariable;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
 
 import org.opencv.core.Mat;
 
 public class LessonZeroVision extends VisionModule {
 
     public void run(Mat frame) {
-        // Your code here
-        // Remember to release your Mats before setting them to something else!
-        // You may create helper functions if you wish
-        // You can also create variables outside this function if necessary
+        postImage(frame, "Original Picture");
+        
+        Mat hsvFrame = new Mat();
+        Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
+        postImage(hsvFrame, "HSV Converted Image");
+
+        Mat hueFiltered = new Mat();
+        ArrayList<Mat> hues = new ArrayList<Mat>();
+        Core.split(frame, hues);
+        Core.inRange(hues.get(0), new Scalar(86), new Scalar(93), hueFiltered);
+        //Core.merge(hues,hueFiltered);
+        postImage(hueFiltered, "Hue Filtered Image");
+
+        Mat filterImage2 = new Mat();
+        ArrayList<Mat> splitImg = new ArrayList<Mat>();
+        Core.split(hueFiltered, splitImg);
+        Core.inRange(splitImg.get(2), new Scalar(83), new Scalar(255), filterImage2);
+        postImage(filterImage2,"Partly Filtered Image");
+        
+        ArrayList<Mat> channel = new ArrayList<Mat>();
+        Mat filteredImage = new Mat();
+        Core.split(frame, channel);
+        Core.inRange(channel.get(0), new Scalar(86), new Scalar(93), channel.get(0));
+        Core.inRange(channel.get(1), new Scalar(0), new Scalar(255), channel.get(1));
+        Core.inRange(channel.get(2), new Scalar(83), new Scalar(255), channel.get(2));
+        Core.merge(channel, filteredImage);
+        postImage(filteredImage, "Filtered Image");
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -12,41 +12,33 @@ public class Main {
     public static void main(String[] args) {
         ModuleRunner runner = new ModuleRunner(5);
         processLessonZero(runner);
+        //processCamera(runner);
         VisionGui.begin(args, runner);
     }
 
     public static void processCamera(ModuleRunner runner) {
         runner.addMapping(new DeviceCaptureSource(0), new Vision());
-        runner.addMapping(new ImageCaptureSource("sampleImages/colorwheel.png"), new Vision());
+        runner.addMapping(new ImageCaptureSource(imagesDir() + "colorwheel.png"), new Vision());
     }
 
     public static void processSamples(ModuleRunner runner) {
-        String imagesDir = Main.class.getResource("").getPath() + "sampleImages/";
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            imagesDir = imagesDir.substring(1); // Chop off leading / that appears before C:
-        }
-        System.out.println("Getting images from " + imagesDir);
-        File directory = new File(imagesDir);
+        System.out.println("Getting images from " + imagesDir());
+        File directory = new File(imagesDir());
         File[] directoryListing = directory.listFiles();
         for (int i = 16; i < directoryListing.length && i < 20; i++) {
             // NOTE: there is no 1.jpg or 2.jpg in the sample images. There is a 0.jpg
-            String path = imagesDir + directoryListing[i].getName();
+            String path = imagesDir() + directoryListing[i].getName();
             runner.addMapping(new ImageCaptureSource(path), new Vision());
         }
-        String colorwheel = imagesDir + "colorwheel.png";
+        String colorwheel = imagesDir() + "colorwheel.png";
         runner.addMapping(new ImageCaptureSource(colorwheel), new Vision());
     }
 
     public static void processLessonZero(ModuleRunner runner) {
-        String imagesDir = Main.class.getResource("").getPath();
-        imagesDir = imagesDir.substring(0, imagesDir.length() - 6);
-        imagesDir += "sampleImages/lesson0/";
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            imagesDir = imagesDir.substring(1); // Chop off leading / that appears before C:
-        }
-        File[] directoryListing = listImagesInDir(imagesDir);
+        File[] directoryListing = listImagesInDir(imagesDir() + "lesson0/");
         // Use the function below to list the images for lesson 0
         // And then map all the images, using your class to process it.
+        runner.addMapping(new ImageCaptureSource(imagesDir() + "lesson0/0.jpg"), new LessonZeroVision());
     }
 
     // path is a relative path
@@ -55,5 +47,15 @@ public class Main {
         System.out.println("Getting images from " + imagesDir);
         File directory = new File(imagesDir);
         return directory.listFiles();
+    }
+
+    public static String imagesDir() {
+        String imagesDir = Main.class.getResource("").getPath();
+        imagesDir = imagesDir.substring(0, imagesDir.length() - 6);
+        imagesDir += "sampleImages/";
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            imagesDir = imagesDir.substring(1); // Chop off leading / that appears before C:
+        }
+        return imagesDir;
     }
 }
