033df62205f1adbc8e4e2581b9fe957e666fb52a
GeoLiang90 <gliang90@stuy.edu>
2018-10-11 01:19:42 -0400

    Fixed code and finished lesson0

--- src/Vision.java
+++ src/Vision.java
@@ -13,28 +13,27 @@ import org.opencv.core.MatOfPoint;
 
 public class Vision extends VisionModule {
 
-	public IntegerSliderVariable minHue = new IntegerSliderVariable("Min Hue", 86,  0, 255);
+	public IntegerSliderVariable minHue = new IntegerSliderVariable("Min Hue", 0,  0, 255);
 	//Default, Minimum, Maximum
-  public IntegerSliderVariable maxHue = new IntegerSliderVariable("Max Hue", 93, 0, 255);
+  public IntegerSliderVariable maxHue = new IntegerSliderVariable("Max Hue", 255, 0, 255);
 
-	public IntegerSliderVariable minSat = new IntegerSliderVariable("Min Saturation", 0,  0, 255);
+	public IntegerSliderVariable Sat = new IntegerSliderVariable("Min Saturation", 0,  0, 255);
 	//Default, Minimum, Maximum
-	public IntegerSliderVariable maxSat = new IntegerSliderVariable("Max Saturation", 255, 0, 255);
 
-	public IntegerSliderVariable minVal = new IntegerSliderVariable("Min Value", 0,  0, 83);
-	//Default, Minimum, Maximum
-	public IntegerSliderVariable maxVal = new IntegerSliderVariable("Max Value", 255, 0, 255);
+	public IntegerSliderVariable Val = new IntegerSliderVariable("Max Value", 255, 0, 255);
 
 
 	public void run(Mat frame) {
 		postImage(frame, "Sample");
 
-		Imgproc.cvtColor(frame,frame,Imgproc.COLOR_BGR2HSV);
+		Mat converted = new Mat();
+
+		Imgproc.cvtColor(frame,converted,Imgproc.COLOR_BGR2HSV);
 		ArrayList<Mat> channels = new ArrayList<Mat>();
-		Core.split(frame, channels);
+		Core.split(converted, channels);
 		Core.inRange(channels.get(0),new Scalar(minHue.value()), new Scalar(maxHue.value()),channels.get(0));
-		Core.inRange(channels.get(1),new Scalar(minSat.value()), new Scalar(maxSat.value()),channels.get(1));
-		Core.inRange(channels.get(2),new Scalar(minVal.value()),new Scalar(maxVal.value()),channels.get(2));
+		Core.inRange(channels.get(1),new Scalar(Sat.value()), new Scalar(255),channels.get(1));
+		Core.inRange(channels.get(2),new Scalar(Val.value()),new Scalar(255),channels.get(2));
 
 		postImage(channels.get(0), "Hue Filtered");
 		postImage(channels.get(1), "Saturation Filtered");
@@ -44,14 +43,18 @@ public class Vision extends VisionModule {
 		Mat combined = new Mat();
 
 		Core.bitwise_and(channels.get(0),channels.get(1),hs);
+
+		postImage(hs, "Hue and Saturation Filtered");
+		
 		Core.bitwise_and(channels.get(2),hs,combined);
 
 		postImage(combined, "Combined Filtered");
+
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Mat hierarchy = new Mat();
+		
 		Mat drawn = frame.clone();
-		Imgproc.findContours(combined,contours,hierarchy,Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_SIMPLE);
-		Imgproc.drawContours(drawn,contours,-1,new Scalar(0,255,0),1);
+		Imgproc.findContours(combined,contours,new Mat(),Imgproc.RETR_EXTERNAL,Imgproc.CHAIN_APPROX_SIMPLE);
+		Imgproc.drawContours(drawn,contours,-1,new Scalar(0,255,0),3);
 		postImage(drawn,"Contoured");
 
 		//Imgproc.
